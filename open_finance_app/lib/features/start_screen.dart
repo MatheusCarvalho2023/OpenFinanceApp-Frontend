83fc4de3dfb618dec6db8363b4b61ad32e88d900
Mauricio Lechuga <mlechugasanchez2857@conestogac.on.ca>
2025-02-13 21:19:22 -0500

    Refactor SignupScreen: Update button text to 'Login'

--- open_finance_app/lib/features/start_screen.dart
+++ open_finance_app/lib/features/start_screen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: use_build_context_synchronously, avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:open_finance_app/theme/colors.dart';
 import 'package:open_finance_app/widgets/buttons/primary_button.dart';
 import 'package:open_finance_app/widgets/buttons/secondary_button.dart';
 import 'package:open_finance_app/features/signup_screen.dart';
