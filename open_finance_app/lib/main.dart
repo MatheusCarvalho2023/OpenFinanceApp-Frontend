25aafe7da6ab6a7f0dcfcd1d4fb90aacd2a9e74c
Mauricio Lechuga <mlechugasanchez2857@conestogac.on.ca>
2025-03-27 15:43:48 -0400

    Update import path for login screen in main.dart

--- open_finance_app/lib/main.dart
+++ open_finance_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:open_finance_app/features/profile/profile_home.dart';
 import 'package:open_finance_app/features/connections/connections_screen.dart';
-import 'package:open_finance_app/features/wallet/login_screen.dart';
+import 'package:open_finance_app/features/login/login_screen.dart';
 import 'package:open_finance_app/features/wallet/summary_screen.dart';
 import 'package:open_finance_app/navigation/main_navigation.dart';
 import 'package:open_finance_app/theme/colors.dart';
